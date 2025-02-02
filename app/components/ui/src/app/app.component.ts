b726255928138e1abeee941227ea9658fc2a2a66
Jason Esterhuizen <jayfalls@users.noreply.github.com>
2025-02-02 11:53:36 +0200

    [REFACT] Restructured ui

--- app/components/ui/src/app/app.component.ts
+++ app/components/ui/src/app/app.component.ts
@@ -1,22 +1,78 @@
-import { Component, OnInit } from "@angular/core";
+// DEPENDENCIES
+//// Angular
+import { Component, OnInit, signal } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { RouterModule, RouterOutlet } from "@angular/router";
 import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+//// Local
 import { appActions } from "./store/actions/app.actions";
-import { ACERootpageComponent } from "./components/rootpage/rootpage.component";
+import { selectAppState } from './store/selectors/app.selectors';
+import { AppState } from './store/state/app.state';
 
+
+// TYPES
+export type SidebarItem = {
+  name: string,
+  icon: string,
+  route?: string,
+}
+
+
+// COMPONENT
 @Component({
   selector: "app-root",
   imports: [
-    ACERootpageComponent
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    RouterModule,
+    RouterOutlet
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss"
 })
 export class AppComponent implements OnInit {
+  // Variables
   title = "ACE";
+  sidebarItems = signal<SidebarItem[]>([
+    {
+      name: "Home",
+      icon: "home",
+      route: ""
+    },
+    {
+      name: "Dashboard",
+      icon: "dashboard",
+      route: "dashboard"
+    },
+    {
+      name: "Chat",
+      icon: "chat",
+      route: "chat"
+    },
+    {
+      name: "Settings",
+      icon: "settings",
+      route: "settings"
+    }
+  ])
+  versionData$: Observable<AppState>;
+  version: string = "0";
 
-  constructor(private store: Store) {}
+  // Initialisation
+  constructor(private store: Store) {
+    this.versionData$ = this.store.select(selectAppState);
+  }
 
   ngOnInit(): void {
       this.store.dispatch(appActions.getACEVersionData());
+      this.versionData$.subscribe( versionData => this.version = versionData.versionData.version);
   }
 }
--- app/components/ui/src/main.ts
+++ app/components/ui/src/main.ts
@@ -1,6 +1,9 @@
+// DEPENDENCIES
+//// Angular
 import { bootstrapApplication } from '@angular/platform-browser';
+//// Local
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
